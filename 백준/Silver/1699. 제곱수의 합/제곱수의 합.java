b034d5072d5bd15e4e6a1a943197995b20ed201a
한민욱 <136693772+wookie3739@users.noreply.github.com>
2024-11-08 14:28:31 +0900

    [Silver II] Title: 제곱수의 합, Time: 156 ms, Memory: 16112 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static int[] dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
+        
+        int result = square_number(N);
+
+        bw.write(result + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int square_number(int N){
+        for (int i = 1; i <= N; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[N];
+    }
+}
