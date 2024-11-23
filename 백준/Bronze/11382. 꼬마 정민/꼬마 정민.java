eed85a705053bc0e3c03cc45d8df1b18728f679a
한민욱 <136693772+wookie3739@users.noreply.github.com>
2024-11-23 16:20:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 116 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        bw.write(a + b + c + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
