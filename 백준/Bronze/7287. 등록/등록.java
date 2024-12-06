441d68d290c6caf2f36db7b899a807f40e032d49
한민욱 <136693772+wookie3739@users.noreply.github.com>
2024-12-06 11:56:57 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+       System.out.println("122" + "\n" + "dnlsemtmf");
+
+    }
+    
+}
