fd840d64140e24e71c13ad41d06895334ec97197
한민욱 <136693772+wookie3739@users.noreply.github.com>
2024-12-25 16:29:26 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+
+            sequence(start, end);
+
+        }
+        for (int num : list) {
+            bw.write(num + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void sequence(int start, int end) {
+
+        while (start < end) {
+            int temp = list.get(start);
+            list.set(start, list.get(end));
+            list.set(end, temp);
+            start++;
+            end--;
+        }
+    }
+
+}
