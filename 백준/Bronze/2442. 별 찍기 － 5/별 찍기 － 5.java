33660d10f381a5037143396fb8bebc4aa520ff64
한민욱 <136693772+wookie3739@users.noreply.github.com>
2024-12-27 16:27:13 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 112 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-//별 찍기 - 5 2442번
+// 별 찍기 - 5 (2442번)
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,11 +12,27 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= N; i++){
-            bw.write(" ".repeat(N - i) + "*".repeat((i * 2) - 1) + "\n");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            result.append(repeatChar(' ', N - i));         // 공백 반복
+            result.append(repeatChar('*', (i * 2) - 1));  // 별 반복
+            result.append("\n");                      // 줄바꿈
         }
+
+        bw.write(result.toString());  // 결과 출력
         bw.flush();
+
         bw.close();
         br.close();
+    }
+
+    // 특정 문자를 count 횟수만큼 반복하는 문자열 반환 메서드
+    public static String repeatChar(char c, int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(c);  // char 값을 추가
+        }
+        return sb.toString();
     }
 }
